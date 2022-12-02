e5eb68aae7d5b4f110a0f46e2c34d659f4936f54
Eduardo Pontes <eduardo.pontes2801@gmail.com>
2022-12-02 13:37:07 -0300

    /feat-frontEndFeatures
    Connecting the app with Material-UI and organizing the visual part of the project.

---
+++ vitrineFullstackApp/client/src/components/product/product-list-toolbar.js
@@ -0,0 +1,51 @@
+import {
+  Box,
+  Button,
+  Card,
+  CardContent,
+  TextField,
+  InputAdornment,
+  SvgIcon,
+  Typography
+} from '@mui/material';
+import { Download as DownloadIcon } from '../../icons/download';
+import { Search as SearchIcon } from '../../icons/search';
+import { Upload as UploadIcon } from '../../icons/upload';
+
+export const ProductListToolbar = (props) => (
+  <Box {...props}>
+    <Box
+      sx={{
+        alignItems: 'center',
+        display: 'flex',
+        justifyContent: 'space-between',
+        flexWrap: 'wrap',
+        m: -1
+      }}
+    >
+      <Typography
+        sx={{ m: 1 }}
+        variant="h4"
+      >
+        Carros usados
+      </Typography>
+      {/* <Box sx={{ m: 1 }}>
+        <Button
+          color="primary"
+          variant="contained"
+        >
+          Adicionar carros
+        </Button>
+      </Box> */}
+    </Box>
+    <Box sx={{ mt: 3 }}>
+      <Card>
+        <CardContent>
+          <Box sx={{ maxWidth: 500 }}>
+            QTDE. CARROS (3.552 Resultados)
+          </Box>
+        </CardContent>
+      </Card>
+    </Box>
+  </Box>
+);
